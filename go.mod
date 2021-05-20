f3372c2c80f168026797eb81fae09d8d41a2236b
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-05-20 13:45:28 -0500

    Hmac functions

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cmd-ctrl-q/golang-arch
 
 go 1.15
 
-require golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+require (
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/tools v0.1.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"fmt"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+var secret []byte
+
 // `curl -u username:password -v domain.com`
 func main() {
+
+	// shoud generate secret key with random generator
+	for i := 0; i < 64; i++ {
+		secret = append(secret, byte(i))
+	}
+
 	pass := "123456789"
 
 	hashedPass, err := hashPassword(pass)
@@ -40,4 +50,26 @@ func comparePassword(password string, hashedPass []byte) error {
 		return fmt.Errorf("Invalid password: %w", err)
 	}
 	return nil
+}
+
+func signMessage(msg []byte) ([]byte, error) {
+	hash := hmac.New(sha512.New, secret)
+	_, err := hash.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("Error in signMessage while hashing message: %w", err)
+	}
+
+	signature := hash.Sum(nil)
+
+	return signature, nil
+}
+
+func checkSignature(msg, sig []byte) (bool, error) {
+	newSignature, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("Error in checkSig while getting signature of message: %w", err)
+	}
+
+	same := hmac.Equal(newSignature, sig)
+	return same, nil
 }
