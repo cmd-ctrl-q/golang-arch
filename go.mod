db66d7ae56ab32d84e42531eab969e305e722280
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-05-19 23:32:52 -0500

    Hashing example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cmd-ctrl-q/golang-arch
 
 go 1.15
+
+require golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // `curl -u username:password -v domain.com`
 func main() {
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+	pass := "123456789"
+
+	hashedPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
+
+	err = comparePassword(pass, hashedPass)
+	if err != nil {
+		log.Fatalln("Not logged in")
+	}
+
+	log.Println("Logged in!")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error while generating bcrypt bash from password %w", err)
+	}
+
+	return bs, nil
+}
+
+// comparing a hash with the original pass code is a potential source of vulnerabilities.
+func comparePassword(password string, hashedPass []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+	if err != nil {
+		return fmt.Errorf("Invalid password: %w", err)
+	}
+	return nil
 }
