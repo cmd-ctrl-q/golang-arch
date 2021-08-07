467953444e3f8a10a18ab8f96d3d3660c30494e4
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-07 17:35:49 -0500

    Revert "using Is() and As() for error handling"
    This reverts commit 4fd12d7f77058348159613586c02c3a0f404abcc.

--- main.go
+++ main.go
@@ -3,27 +3,21 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
 
 	f, err := os.Open("file-01.txt")
-	if errors.Is(err, os.ErrPermission) {
-		var pErr *os.PathError
-		if errors.As(err, &pErr) {
-			err = fmt.Errorf("permission denied : %w", pErr)
-		} else {
-			err = fmt.Errorf("permission denied : %w", err)
-		}
-		log.Println(err)
-	} else if errors.Is(err, os.ErrNotExist) {
+	if errors.Is(err, os.ErrNotExist) {
 		err = fmt.Errorf("error not exist : %w", err)
-		log.Println(err)
-	} else if !errors.Is(err, nil) {
-		err = fmt.Errorf("file could not be opened : %w", err)
-		log.Println(err)
+	}
+	if errors.Is(err, os.ErrPermission) {
+		err = fmt.Errorf("permission denied : %w", err)
+	}
+	if !errors.Is(err, nil) {
+		err = fmt.Errorf("another error : %w", err)
 	}
 	defer f.Close()
+	fmt.Println("Stack error:\n", err)
 }
