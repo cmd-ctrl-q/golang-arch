4fd12d7f77058348159613586c02c3a0f404abcc
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-07 17:31:08 -0500

    using Is() and As() for error handling

--- main.go
+++ main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 
 	f, err := os.Open("file-01.txt")
-	if errors.Is(err, os.ErrNotExist) {
-		err = fmt.Errorf("error not exist : %w", err)
-	}
 	if errors.Is(err, os.ErrPermission) {
-		err = fmt.Errorf("permission denied : %w", err)
-	}
-	if !errors.Is(err, nil) {
-		err = fmt.Errorf("another error : %w", err)
+		var pErr *os.PathError
+		if errors.As(err, &pErr) {
+			err = fmt.Errorf("permission denied : %w", pErr)
+		} else {
+			err = fmt.Errorf("permission denied : %w", err)
+		}
+		log.Println(err)
+	} else if errors.Is(err, os.ErrNotExist) {
+		err = fmt.Errorf("error not exist : %w", err)
+		log.Println(err)
+	} else if !errors.Is(err, nil) {
+		err = fmt.Errorf("file could not be opened : %w", err)
+		log.Println(err)
 	}
 	defer f.Close()
-	fmt.Println("Stack error:\n", err)
 }
