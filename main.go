87f0f8f04e883086b233ee438a5098d58725547c
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-08 16:18:50 -0500

    errors Is() and As() with else if

--- main.go
+++ main.go
@@ -1,23 +1,43 @@
+/*
+
+Assertion with errors.As()
+
+Assert errors to determine which type they are in order to
+better diagnose the problem and return descriptive logs.
+
+Also can access fields or methods assocaited with another
+error type. Assert if this error is also of this other type,
+which will give us access to the methods and fields.
+
+`errors.As(err error, target interface{})`
+
+*/
 package main
 
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 
 	f, err := os.Open("file-01.txt")
-	if errors.Is(err, os.ErrNotExist) {
-		err = fmt.Errorf("error not exist : %w", err)
-	}
 	if errors.Is(err, os.ErrPermission) {
-		err = fmt.Errorf("permission denied : %w", err)
-	}
-	if !errors.Is(err, nil) {
-		err = fmt.Errorf("another error : %w", err)
+		var pErr *os.PathError
+		if errors.As(err, &pErr) {
+			err = fmt.Errorf("permission denied : %w", pErr)
+		} else {
+			err = fmt.Errorf("permission denied : %w", err)
+		}
+		log.Println(err)
+	} else if errors.Is(err, os.ErrNotExist) {
+		err = fmt.Errorf("error not exist : %w", err)
+		log.Println(err)
+	} else if !errors.Is(err, nil) {
+		err = fmt.Errorf("file could not be opened : %w", err)
+		log.Println(err)
 	}
 	defer f.Close()
-	fmt.Println("Stack error:\n", err)
 }
