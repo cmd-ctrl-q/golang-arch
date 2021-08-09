4d230768fce26ed64263d0f99de5d1046e9034d7
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-08 20:30:50 -0500

    stringer

--- main.go
+++ main.go
@@ -3,36 +3,22 @@ Stop writing so many `if err != nil` statements.
 */
 package main
 
-import (
-	"errors"
-	"fmt"
+import "fmt"
 
-	"github.com/cmd-ctrl-q/golang-arch/fileWriter"
-)
+type person struct {
+	pseudonym string
+}
+
+func (p person) String() string {
+	return fmt.Sprintf("my name is %s", p.pseudonym)
+}
 
 func main() {
 
-	f := fileWriter.NewWriteFile("file.txt")
-	f.WriteString("line 1\n")
-	f.WriteString("line 2\n")
-	f.WriteString("line 3\n")
-	f.WriteString("line 4\n")
-	f.Close()
-
-	err := f.Err()
-	if err != nil {
-
-		var fErr *fileWriter.WriteFileError
-		if errors.As(err, &fErr) {
-
-			if fErr.Op == "NewWriteFile-Create" {
-				fmt.Println("Error occured at NewWriteFile()")
-			}
-		} else {
-			fmt.Println("Could not assert err as &fErr")
-		}
-	} else {
-		fmt.Println("No error!")
+	p := person{
+		pseudonym: "batman",
 	}
 
+	fmt.Println(p) // my name is batman
+	fmt.Print(p)   // my name is batman
 }
